9bae2618e730f098112191f31368838b19ed58ec
André Brás <andrebras@gmail.com>
2019-04-12 15:51:11 +0100

    Connect to mongodb

--- bee.js
+++ bee.js
@@ -4,3 +4,20 @@ http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello World\n');
 }).listen(process.env.PORT || 8080, () => console.log('Server started'));
+
+const MongoClient = require('mongodb').MongoClient;
+const assert      = require('assert');
+const url         = 'mongodb://localhost:27017';
+
+const dbName = 'duty-bee';
+const client = new MongoClient(url, { useNewUrlParser: true });
+
+client.connect((err) => {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db         = client.db(dbName);
+  const collection = db.collection('settings');
+
+  client.close();
+});
